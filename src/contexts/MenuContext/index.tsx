92333bca20381caf9facfc356a30c0477f969b49
Edu <prog.syrius@gmail.com>
2023-07-29 12:48:45 -0300

    feat: add search states to context

--- src/contexts/MenuContext/index.tsx
+++ src/contexts/MenuContext/index.tsx
@@ -5,17 +5,35 @@ import { createContext, useState } from "react";
 interface IMenuContextProps {
   selectedTag: FoodCategory | null;
   setSelectedTag: (tag: FoodCategory | null) => void;
+  searchIsOpen: boolean;
+  handleSearchToggle: (state: boolean) => void;
+  searchValue: string;
+  setSearchValue: (value: string) => void;
 }
 
 export const MenuContext = createContext<IMenuContextProps>({
   selectedTag: null,
-  setSelectedTag: () => {},
+  searchIsOpen: false,
+  setSelectedTag: () => { },
+  handleSearchToggle: (state) => { },
+  searchValue: '',
+  setSearchValue: () => { },
 });
 
 const MenuProvider = ({ children }: { children: React.ReactNode }) => {
   const [selectedTag, setSelectedTag] = useState<FoodCategory | null>(null);
+  const [searchIsOpen, setSearchIsOpen] = useState(false);
+  const [searchValue, setSearchValue] = useState<string>('');
+  const handleSearchToggle = (state: boolean) => {
+    setSearchIsOpen(state);
+    if (selectedTag !== null) {
+      setSelectedTag(null)
+    }
+  };
   return (
-    <MenuContext.Provider value={{ selectedTag, setSelectedTag }}>
+    <MenuContext.Provider
+      value={{ selectedTag, searchIsOpen, searchValue, setSelectedTag, handleSearchToggle, setSearchValue }}
+    >
       {children}
     </MenuContext.Provider>
   );
