3e813446168a28d2cc309425073e3bd611bb4d4d
Edu <prog.syrius@gmail.com>
2023-07-29 05:12:06 -0300

    feat: add Initial food CategoryTag

---
+++ src/components/CategoryTag.tsx
@@ -0,0 +1,26 @@
+import { FoodCategory } from "@prisma/client";
+
+interface CategoryTagProps {
+  foodCategory: FoodCategory;
+  isSelected: boolean;
+  onClick: () => void;
+}
+const CategoryTag = ({
+  foodCategory,
+  isSelected,
+  onClick,
+}: CategoryTagProps) => {
+  const tagClasses = isSelected
+    ? "bg-specialOrange text-white "
+    : "bg-veryLightGray text-black";
+  return (
+    <div
+      onClick={onClick}
+      className={`text-xs py-1 px-5 whitespace-nowrap rounded-lg cursor-pointer ${tagClasses}`}
+    >
+      {foodCategory.name}
+    </div>
+  );
+};
+
+export default CategoryTag;
