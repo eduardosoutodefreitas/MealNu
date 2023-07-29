69f0aff5d2c3dea97cb92f1e14a8a0f3e099e26a
Edu <prog.syrius@gmail.com>
2023-07-29 10:59:31 -0300

    feat: add Menu Component

---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,9 @@
+import { prisma } from "@/lib/prisma";
+import MenuList from "./MenuList";
+
+const Menu = async () => {
+  const dishes = await prisma.dish.findMany({});
+  return <MenuList dishes={dishes} />;
+};
+
+export default Menu;
