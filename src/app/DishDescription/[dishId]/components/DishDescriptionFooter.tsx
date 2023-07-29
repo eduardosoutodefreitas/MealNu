7b163e9f1c83556d52b24730dde679065598f55e
Edu <prog.syrius@gmail.com>
2023-07-29 09:36:03 -0300

    feat: add Product Page

---
+++ src/app/DishDescription/[dishId]/components/DishDescriptionFooter.tsx
@@ -0,0 +1,12 @@
+interface DishDescriptionFooterProps {
+  price: string;
+}
+const DishDescriptionFooter = ({ price }: DishDescriptionFooterProps) => {
+  return (
+    <div className="mx-auto py-3 flex items-center justify-center border-t border-lightGray/50 w-[95%]">
+      <h4 className="font-semibold">Preço: R${price}</h4>
+    </div>
+  );
+};
+
+export default DishDescriptionFooter;
---
+++ src/app/DishDescription/[dishId]/components/DishDescriptionHeader.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import { BsArrowLeftShort } from "react-icons/bs";
+const DishDescriptionHeader = () => {
+  return (
+    <header className="px-5 h-[10vh] flex items-center ">
+      <Link href="/">
+        <BsArrowLeftShort size={30} className="cursor-pointer" />
+      </Link>
+      <p className="font-medium mx-auto">Descrição do produto</p>
+    </header>
+  );
+};
+
+export default DishDescriptionHeader;
