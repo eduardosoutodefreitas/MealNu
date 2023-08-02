6fa983355fe3bc972aeb788a08f6179374f8f3a6
Edu <prog.syrius@gmail.com>
2023-08-02 11:35:05 -0300

    feat: add Link to Header Component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="container mx-auto h-[10vh] flex items-center">
-      <div className="relative h-[40px] w-[40px]">
-        <Image
-          src="/logo.png"
-          style={{
-            objectFit: "cover",
-          }}
-          fill
-          alt="logo"
-        />
-      </div>
+      <Link href="/" className="flex items-center gap-2">
+        <div className="relative h-[40px] w-[40px]">
+          <Image
+            src="/logo.png"
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            alt="logo"
+          />
+        </div>
+        <p className="font-bold text-lg">Meal
+          <span className="text-specialOrange">Nu</span>
+        </p>
+      </Link>
     </header>
   );
 };
