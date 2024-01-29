276b2a0869e63d202f46daf9bce823f0c77d8289
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-29 10:14:04 -0300

    style: remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 import Banner from "./components/Banner";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import MenuNavigation from "./components/MenuNavigation";
 
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto px-5">
+    <div className='max-w-4xl mx-auto'>
       <Header />
       <Banner />
       <MenuNavigation />
       <Menu />
-      <Footer />
     </div>
   );
 }
