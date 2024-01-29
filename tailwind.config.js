509fbd9f6140604c568dc9f60a9eab89d4e8a5e2
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-29 10:13:30 -0300

    chore: add new breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "372px",
+      },
       colors: {
         specialOrange: "#F16717",
         lightGray: "#767373",
         veryLightGray: "#F1F1F1",
         darkGray: "#1E1E1E",
-        secundaryDark: "#232121"
+        secundaryDark: "#232121",
       },
     },
   },
-  plugins: [ require('tailwind-scrollbar'),],
+  plugins: [require("tailwind-scrollbar")],
 };
